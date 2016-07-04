ae2f90b31ac51f5735dc70a8bf554afe9b091d81
mgniu <mgniu@dataman-inc.com>
2016-07-04 15:44:14 +0800

    add database creation sql

--- db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mysql -hmysql -uroot -p${MYSQL_ENV_MYSQL_ROOT_PASSWORD} --execute="CREATE DATABASE IF NOT EXISTS alarm;" \
-                                                        --execute="CREATE DATABASE IF NOT EXISTS oapp;" \
-                                                        --execute="CREATE DATABASE IF NOT EXISTS alert;" \
-                                                        --execute="CREATE DATABASE IF NOT EXISTS drone;" \
-                                                        --execute="CREATE DATABASE IF NOT EXISTS billing;" \
-                                                        --execute="CREATE DATABASE IF NOT EXISTS registry;" \
-
